e2bf50c5b2c4810ea0f03eacf653f1aa33304ddc
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-07-01 11:35:16 +0000

    build based on aec04f3

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DaemonMode","category":"page"}]
+[{"location":"quickstart/#Quickstart","page":"Quick Start","title":"Quickstart","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"CurrentModule = DaemonMode","category":"page"},{"location":"#Introduction","page":"Home","title":"Introduction","text":"","category":"section"}]
 }
