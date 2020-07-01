3e0d7739bedc3229560bda37699865fdce88555f
Daniel Molina <dmolina@decsai.ugr.es>
2020-07-01 11:57:59 +0200

    Add Documentation.

---
+++ docs/make.jl
@@ -0,0 +1,21 @@
+using DaemonMode
+using Documenter
+
+makedocs(;
+    modules=[DaemonMode],
+    authors="Daniel Molina <dmolina@decsai.ugr.es>",
+    repo="https://github.com/dmolina/MoodleQuestions.jl/blob/{commit}{path}#L{line}",
+    sitename="DaemonMode.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://dmolina.github.io/DaemonMode.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/dmolina/MoodleQuestions.jl",
+)
