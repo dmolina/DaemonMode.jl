6574bf81fa5e849ab77f205cff0b7d9cda47e4f6
Gauthier Soleilhac <gsoleilhac@gmail.com>
2020-07-08 15:19:52 +0200

    Add runexpr mode

--- src/DaemonMode.jl
+++ src/DaemonMode.jl
@@ -1,5 +1,7 @@
 module DaemonMode
 
+export serve, runfile, runargs, runexpr, sendExitCode
+
 using Sockets
 
 const PORT = 3000
@@ -7,81 +9,119 @@ const PORT = 3000
 function add_packages(fname::AbstractString)
 end
 
-const first_time = [true]
+const first_time = Ref(true)
 const token_end = "DaemonMode::Exit"
 
 function serve(port=PORT)
     server = Sockets.listen(Sockets.localhost, port)
-    global first_time
-    global token_end
     quit = false
     current = pwd()
 
     while !quit
         sock = accept(server)
-        dir = readline(sock)
-        fname = readline(sock)
-        args_str = readline(sock)
-        args = split(args_str, " ")
-
-        for arg in args
-            push!(ARGS, arg)
-        end
-
-        first_time .= true
-        cd(current)
-
-        if (fname == "exit()")
-            println(sock, token_end)
-            sleep(1)
-            quit = true
-            continue
-        end
-
+        mode = readline(sock)
+        
         (_, old) = redirect_stdout()
         redirect_stdout(sock)
         (_, old_error) = redirect_stderr()
         redirect_stderr(sock)
-        error = ""
 
-        try
-            cd(dir)
-            include(joinpath(dir, fname))
-        catch e
-            if isa(e, LoadError)
-                if :msg in propertynames(e.error)
-                    error_msg = e.error.msg
-                else
-                    error_msg = "$e.error"
-                end
-
-                error = "ERROR in line $(e.line): '$(error_msg)'"
-            else
-                error = "ERROR: could not open file '$fname'"
-            end
-        end
-
-        while !isempty(ARGS)
-            pop!(ARGS)
+        if mode == "runFile()"
+            serverRunFile(sock)
+        elseif mode == "runExpr()"
+            serverRunExpr(sock)
+        elseif mode == "exit()"
+            println(sock, token_end)
+            sleep(1)
+            quit = true
+        else
+            println(sock, "Error, unrecognised mode, expected (\"runFile()\", \"runExpr()\" or \"exit()\", but received \"$mode\"")
         end
 
         redirect_stdout(old)
         redirect_stderr(old_error)
+    end
+end
 
-        if !isempty(error)
-            println(sock, error)
+function serverRunFile(sock)
+    dir = readline(sock)
+    fname = readline(sock)
+    args_str = readline(sock)
+    args = split(args_str, " ")
+
+    append!(ARGS, args)
+
+    first_time[] = true
+    error = ""
+
+    try
+        cd(dir) do 
+            include(joinpath(dir, fname))
+        end
+    catch e
+        if isa(e, LoadError)
+            if :msg in propertynames(e.error)
+                error_msg = e.error.msg
+            else
+                error_msg = "$(e.error)"
+            end
+            error = "ERROR in line $(e.line): '$(error_msg)'"
+        else
+            error = "ERROR: could not open file '$fname'"
+        end
+    end
+
+    !isempty(error) && println(sock, error)
+    println(sock, token_end)
+    empty!(ARGS)
+end
+
+function serverRunExpr(sock)
+    dir = readline(sock)
+    expr = readline(sock)
+    error = ""
+
+    try
+        cd(dir) do 
+            Main.eval(Meta.parse(expr))
+        end
+    catch e
+        if isa(e, LoadError)
+            if :msg in propertynames(e.error)
+                error_msg = e.error.msg
+            else
+                error_msg = "$(e.error)"
+            end
+            error = "ERROR in line $(e.line): '$(error_msg)'"
+        else
+            error = "ERROR: $e"
+        end
+    end
+
+    !isempty(error) && println(sock, error)
+    println(sock, token_end)
+    empty!(ARGS)
+end
+
+function runexpr(expr::AbstractString, output = stdout, port = PORT)
+    try
+        sock = Sockets.connect(port)
+        println(sock, "runExpr()")
+        println(sock, pwd())
+        println(sock, expr)
+
+        line = readline(sock)
+        while (line != token_end)
+            println(output, line)
+            line = readline(sock)
         end
 
-        println(sock, token_end)
-
-        while !isempty(ARGS)
-            pop!(ARGS)
-        end
+    catch e
+        println(stderr, "Error, cannot connect with server. Is it running?")
     end
 end
 
 function runfile(fname::AbstractString; args=String[], port=port, output=stdout)
-    global token_end
     dir = dirname(fname)
 
     if isempty(dir)
@@ -92,19 +132,24 @@ function runfile(fname::AbstractString; args=String[], port=port, output=stdout)
 
     try
         sock = Sockets.connect(port)
+        println(sock, "runFile()")
         println(sock, pwd())
         println(sock, fcompletename)
         println(sock, join(args, " "))
         line = readline(sock)
-
         while (line != token_end)
             println(output, line)
             line = readline(sock)
         end
     catch e
-        println(stderr, "Error, cannot connected with server. It is running?")
+        println(stderr, "Error, cannot connect with server. Is it running?")
     end
     return
+end
+
+function sendExitCode(port = PORT)
+    sock = Sockets.connect(port)
+    println(sock, "exit()")
 end
 
 function runargs(port=PORT)
@@ -114,8 +159,5 @@ function runargs(port=PORT)
     runfile(ARGS[1], args=ARGS[2:end], port=port)
 end
 
-export serve
-export runfile
-export runargs
 
 end # module
